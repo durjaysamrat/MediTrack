e6ebe8a88ac9240ba891f44cea057bf9b1b6f9a2
DURJAY SAMRAT NANDAMUDI <111143303+durjaysamrat@users.noreply.github.com>
2025-03-09 00:14:22 +0530

    admin-component

--- proj/src/app/app-routing.module.ts
+++ proj/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { UserComponent } from './user/user.component';
+
+const routes: Routes = [
+  { path: 'admin', component: AdminComponent, 
+    children: [
+    { path: 'users', component: UserComponent },
+    { path: 'billing', component: BillingComponent },
+    { path: 'reports', component: ReportsComponent }
+  ] },
+  
+  { path: '', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- proj/src/app/app.module.ts
+++ proj/src/app/app.module.ts
@@ -4,16 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AdminComponent } from './admin/admin.component';
+import { UserComponent } from './user/user.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AdminComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
